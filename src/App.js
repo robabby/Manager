03eccab198a63d0a888b0f4667255d5481e0363f
robabby <robabby23@gmail.com>
2017-06-01 21:41:40 -0500

    Fleshed out Hello World

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyDFkn-R7Gd3T_fPESb7RngdmoE0uRW_cf0",
+      authDomain: "manager-1a641.firebaseapp.com",
+      databaseURL: "https://manager-1a641.firebaseio.com",
+      projectId: "manager-1a641",
+      storageBucket: "manager-1a641.appspot.com",
+      messagingSenderId: "629186823453"
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
         <View>
           <Text>
-            Hello!
+            Hello World!
           </Text>
         </View>
       </Provider>
