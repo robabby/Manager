38418b786ac91c4d999e82d29519d60762b49c00
robabby <robabby23@gmail.com>
2017-06-02 08:20:37 -0500

    Implemented LoginForm shell

---
+++ src/components/LoginForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Card, CardSection, Input, Button } from './common';
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <Card>
+      </Card>
+    );
+  }
+}
+
+export default LoginForm;
