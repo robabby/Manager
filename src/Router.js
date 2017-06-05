a964cae4e7889dcf6644a53540a0e93a0331f817
robabby <robabby23@gmail.com>
2017-06-05 12:09:56 -0500

    Setting up employee edit and fixing some UI bugs

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import { Actions, Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
 import EmployeeCreate from './components/EmployeeCreate';
+import EmployeeEdit from './components/EmployeeEdit';
 
 const RouterComponent = () => {
   return (
@@ -19,7 +20,8 @@ const RouterComponent = () => {
           title="Employees"
           initial
         />
-      <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" />
+        <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" />
+        <Scene key="employeeEdit" component={EmployeeEdit} title="Edit Employee" />
       </Scene>
     </Router>
   )
--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,16 +1,29 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { EmployeeForm } from './EmployeeForm';
+import EmployeeForm from './EmployeeForm';
+import { employeeUpdate, employeeSave } from '../actions';
 import { Card, CardSection, Button } from './common';
 
-
 class EmployeeEdit extends Component {
+  componentWillMount() {
+    _.each(this.props.employee, (value, prop) => {
+      this.props.employeeUpdate({ prop, value });
+    });
+  }
+
+  onButtonPress() {
+    const { name, phone, shift } = this.props;
+
+    this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
+  }
+
   render() {
     return (
       <Card>
         <EmployeeForm />
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Save Changes
           </Button>
         </CardSection>
@@ -19,4 +32,12 @@ class EmployeeEdit extends Component {
   }
 }
 
-export default connect()(EmployeeEdit);
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm;
+
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, {
+  employeeUpdate, employeeSave
+})(EmployeeEdit);
