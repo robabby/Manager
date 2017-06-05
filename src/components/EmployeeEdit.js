2defe0f17eccaba7ef67eb4edd49a5073ef9a2bd
robabby <robabby23@gmail.com>
2017-06-05 13:09:57 -0500

    Modal successfully dismisses or deletes record

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
-import { employeeUpdate, employeeSave } from '../actions';
-import { Card, CardSection, Button } from './common';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
+import { Card, CardSection, Button, Confirm } from './common';
 
 class EmployeeEdit extends Component {
+  state = { showModal: false };
+
   componentWillMount() {
     _.each(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({ prop, value });
@@ -25,6 +27,16 @@ class EmployeeEdit extends Component {
     Communications.text(phone, `Your upcoming shift is on ${shift}`);
   }
 
+  onAccept() {
+    const { uid } = this.props.employee;
+
+    this.props.employeeDelete({ uid });
+  }
+
+  onDecline() {
+    this.setState({ showModal: false });
+  }
+
   render() {
     return (
       <Card>
@@ -34,12 +46,27 @@ class EmployeeEdit extends Component {
             Save Changes
           </Button>
         </CardSection>
+
         <CardSection>
           <Button onPress={this.onTextPress.bind(this)}>
             Text Schedule
           </Button>
         </CardSection>
-      </Card>
+
+        <CardSection>
+          <Button onPress={() => this.setState({ showModal: !this.state.showModal })}>
+            Delete Record
+          </Button>
+        </CardSection>
+
+      <Confirm
+        visible={this.state.showModal}
+        onAccept={this.onAccept.bind(this)}
+        onDecline={this.onDecline.bind(this)}
+      >
+        Are you sure you want to delete this?
+      </Confirm>
+    </Card>
     );
   }
 }
@@ -51,5 +78,5 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps, {
-  employeeUpdate, employeeSave
+  employeeUpdate, employeeSave, employeeDelete
 })(EmployeeEdit);
